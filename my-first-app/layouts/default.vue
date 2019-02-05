7e3df1cbce40200f7a0f9aec507f8c10e9d1eabc
Leaphengty <34527109+Leaphengty@users.noreply.github.com>
2019-02-05 22:52:58 +0700

    FirstCommit

---
+++ my-first-app/layouts/default.vue
@@ -0,0 +1,114 @@
+<template>
+  <v-app class="pink">
+    <!--<v-navigation-drawer-->
+      <!--v-model="drawer"-->
+      <!--:mini-variant="miniVariant"-->
+      <!--:clipped="clipped"-->
+      <!--app-->
+    <!--&gt;-->
+      <!--<v-list>-->
+        <!--<v-list-tile-->
+          <!--v-for="(item, i) in items"-->
+          <!--:key="i"-->
+          <!--:to="item.to"-->
+          <!--router-->
+          <!--exact-->
+        <!--&gt;-->
+          <!--&lt;!&ndash;<v-list-tile-action>&ndash;&gt;-->
+            <!--&lt;!&ndash;<v-icon>{{ item.icon }}</v-icon>&ndash;&gt;-->
+          <!--&lt;!&ndash;</v-list-tile-action>&ndash;&gt;-->
+          <!--<v-list-tile-content>-->
+            <!--<v-list-tile-title v-text="item.title" />-->
+          <!--</v-list-tile-content>-->
+        <!--</v-list-tile>-->
+      <!--</v-list>-->
+    <!--</v-navigation-drawer>-->
+    <!--<v-toolbar-->
+      <!--:clipped-left="clipped"-->
+      <!--fixed-->
+      <!--app-->
+    <!--&gt;-->
+      <!--<v-toolbar-side-icon @click="drawer = !drawer" />-->
+      <!--<v-btn-->
+        <!--icon-->
+        <!--@click.stop="miniVariant = !miniVariant"-->
+      <!--&gt;-->
+        <!--<v-icon>{{ `chevron_${miniVariant ? 'right' : 'left'}` }}</v-icon>-->
+      <!--</v-btn>-->
+      <!--<v-btn-->
+        <!--icon-->
+        <!--@click.stop="clipped = !clipped"-->
+      <!--&gt;-->
+        <!--<v-icon>web</v-icon>-->
+      <!--</v-btn>-->
+      <!--<v-btn-->
+        <!--icon-->
+        <!--@click.stop="fixed = !fixed"-->
+      <!--&gt;-->
+        <!--<v-icon>remove</v-icon>-->
+      <!--</v-btn>-->
+      <!--<v-toolbar-title v-text="title" />-->
+      <!--<v-btn-->
+        <!--icon-->
+        <!--@click.stop="rightDrawer = !rightDrawer"-->
+      <!--&gt;-->
+        <!--<v-icon>menu</v-icon>-->
+      <!--</v-btn>-->
+
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <!--<v-navigation-drawer-->
+      <!--v-model="rightDrawer"-->
+      <!--:right="right"-->
+      <!--temporary-->
+      <!--fixed-->
+    <!--&gt;-->
+      <!--&lt;!&ndash;<v-list>&ndash;&gt;-->
+        <!--&lt;!&ndash;<v-list-tile @click.native="right = !right">&ndash;&gt;-->
+          <!--&lt;!&ndash;<v-list-tile-action>&ndash;&gt;-->
+            <!--&lt;!&ndash;<v-icon light>compare_arrows</v-icon>&ndash;&gt;-->
+          <!--&lt;!&ndash;</v-list-tile-action>&ndash;&gt;-->
+          <!--&lt;!&ndash;<v-list-tile-title>Switch drawer (click me)</v-list-tile-title>&ndash;&gt;-->
+        <!--&lt;!&ndash;</v-list-tile>&ndash;&gt;-->
+      <!--&lt;!&ndash;</v-list>&ndash;&gt;-->
+    <!--</v-navigation-drawer>-->
+    <v-footer class="pink center--text"
+      app
+    >
+      <span alr>&copy;copy right KhmerDating</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'bubble_chart',
+          title: 'Inspire',
+          to: '/Homepage'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      // title: 'Vuetify.js'
+      title:'KhmerDating',
+      color: 'pink'
+    }
+  }
+}
+</script>
---
+++ my-first-app/pages/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    nothing
+
+  >
+    <!--<v-flex-->
+      <!--xs50-->
+      <!--sm8-->
+      <!--md2-->
+    <!--&gt;-->
+      <div class="text-xs-center">
+        <logo />
+        <vuetify-logo />
+      </div>
+      <v-card>
+          <!--<v-card-title class="headline">Welcome to the paradise to find the special person.</v-card-title>-->
+        <v-card-text>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            color="primary"
+            f="circle"
+            nuxt
+            to="/Homepage"
+          >Countinue</v-btn>
+          <v-btn fab drepressed  dark color="purple">
+            <v-icon>email</v-icon>
+          </v-btn>
+        </v-card-actions>
+
+      </v-card>
+
+    <!--</v-flex>-->
+  </v-layout>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    Logo
+  }
+}
+</script>
